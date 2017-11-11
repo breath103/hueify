09b9745ed03ac80be227e33371b5464350a873ce
Kurt Lee <breath103@gmail.com>
2017-11-12 03:24:39 +0900

    add alarm

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -1,8 +1,9 @@
 import * as Hue from 'node-hue-api';
 const screenshot = require('screenshot-desktop');
 import * as Jimp from 'jimp';
+
 import Vibrant = require('node-vibrant');
-import { setInterval } from 'timers';
+import Schedule = require("node-schedule");
 
 async function main() {
   const bridges = await Hue.upnpSearch(3000);
@@ -38,31 +39,59 @@ async function main() {
     console.log(`Light[${index}] - ${JSON.stringify(light.state, null, 2)}`);
   })
 
-  // Every 1 seconds.
-  console.log("Taking screenshots..");
+  const schedules = await api.schedules();
+  console.log(`Schedules : ${JSON.stringify(schedules, null, 2)}`);
+
+  // const date = new Date();
+  // date.setTime(date.getTime() + 1500);
+
+  // // Every 1 seconds.
+  // console.log("Taking screenshots..");
   const setLightsState = async (state: Hue.lightState.State) => {
     await Promise.all(
       lights.map(async (light) => {
-        if (light.id && light.state.on) {
+        if (light.id && light.state.reachable) {
           await api.setLightState(light.id, state);
         }
       })
     );
   };
 
-
-  setInterval(async () => {
-    const imgBuffer = await screenshot();
-    const jimp = await Jimp.read(imgBuffer);
-    const vibrant = Vibrant.from(imgBuffer);
-
-    const swatches = await vibrant.getSwatches();
-    if (swatches.Muted) {
-      const color = swatches.Muted.getRgb();
-      const newState = Hue.lightState.create().rgb(color[0], color[1], color[2]).transitionTime(4);
-      await setLightsState(newState);
+  const date = new Date(2017, 11, 12, 9, 50, 0);
+  // const date = (new Date());
+  // date.setTime(date.getTime() + 1000);
+  Schedule.scheduleJob(date, async () => {
+    console.log("ALARM GOES OFF");
+    const setLight = async () => {
+      const state =
+      Hue.lightState.create()
+        .alertLong()
+        .effect("none")
+        .hsb(338, 100, 50)
+        .turnOn();
+      await setLightsState(state);
     }
-  }, 500);
+
+    setInterval(() => {
+      setLight();
+    }, 10000);
+  });
+
+
+  // setTimeout(async () => {
+  // }, 1500);
+  // setInterval(async () => {
+  //   const imgBuffer = await screenshot();
+  //   const jimp = await Jimp.read(imgBuffer);
+  //   const vibrant = Vibrant.from(imgBuffer);
+
+  //   const swatches = await vibrant.getSwatches();
+  //   if (swatches.Muted) {
+  //     const color = swatches.Muted.getRgb();
+  //     const newState = Hue.lightState.create().rgb(color[0], color[1], color[2]).transitionTime(4);
+  //     await setLightsState(newState);
+  //   }
+  // }, 500);
 }
 
 
